0c8c8d324b6e664e6482042dfc0a9c3091fed206
gpuzikov <bythetort@gmail.com>
2025-03-08 18:25:38 +0300

    сделал так, чтобы redis host брался из env

--- celery_settings.py
+++ celery_settings.py
@@ -1,9 +1,11 @@
 from celery import Celery
 import os
 
+
+print({os.getenv('REDIS_HOST')})
 celery = Celery(
-    broker=f"redis://redis:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DATABASES')}",
-    backend=f"redis://redis:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DATABASES')}",
-    include=["methods", "tasks"],
+    broker=f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DATABASES')}",
+    backend=f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DATABASES')}",
+    include=["methods", "tasks", "products.products"],
     timezone="Europe/Moscow",
 )
--- db.py
+++ db.py
@@ -3,4 +3,4 @@ import redis
 import os
 
 db = SQLAlchemy()
-r = redis.Redis(host="redis", port=int(os.getenv("REDIS_PORT")), db=0)
+r = redis.Redis(host={os.getenv('REDIS_HOST')}, port=int(os.getenv("REDIS_PORT")), db=0)
