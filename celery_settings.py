d75dde95ae10e018647b25e4e53e78b5221f970c
gpuzikov <bythetort@gmail.com>
2025-03-16 22:24:43 +0300

    fixed redis security breach

--- celery_settings.py
+++ celery_settings.py
@@ -4,8 +4,8 @@ import os
 
 print({os.getenv('REDIS_HOST')})
 celery = Celery(
-    broker=f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DATABASES')}",
-    backend=f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DATABASES')}",
+    broker=f"redis://:{os.getenv('REDIS_PASSWORD')}@{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DATABASES')}",
+    backend=f"redis://:{os.getenv('REDIS_PASSWORD')}@{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DATABASES')}",
     include=["methods", "tasks", "products.products"],
     timezone="Europe/Moscow",
 )
