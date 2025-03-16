17dfae236b78e1cfed9b67301feb31fba6a1bdb4
gpuzikov <bythetort@gmail.com>
2025-03-16 22:24:43 +0300

    fixed redis security breach

--- db.py
+++ db.py
@@ -3,4 +3,9 @@ import redis
 import os
 
 db = SQLAlchemy()
-r = redis.Redis(host={os.getenv('REDIS_HOST')}, port=int(os.getenv("REDIS_PORT")), db=0)
+r = redis.Redis(
+    host=os.getenv('REDIS_HOST'),
+    port=os.getenv('REDIS_PORT'),
+    password=os.getenv('REDIS_PASSWORD'),
+    db=0
+)
