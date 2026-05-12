7d6c70825e923551a52631c4d4291e8ec6303989
gpuzikov <bythetort@gmail.com>
2026-05-12 09:39:26 +0300

    feat: add CreateNewProductView wrapper

---
+++ frontend/src/views/CreateNewProductView.vue
@@ -0,0 +1,11 @@
+<template>
+    <CreateProductPanama />
+  </template>
+
+  <script>
+  import CreateProductPanama from "@/components/CreateProductPanama.vue";
+
+  export default {
+    components: { CreateProductPanama },
+  };
+  </script>
